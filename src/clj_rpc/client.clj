46f3eb659c12a0ce592375d40e7e8d37282bb210
Martin <zxm@basecity.com>
2011-11-10 14:43:15 +0800

    implemnt client multi invoke, add multi invoke test

--- src/clj_rpc/client.clj
+++ src/clj_rpc/client.clj
@@ -13,6 +13,10 @@
 ;; to invoke a remote function + with parameters
 ;;
 ;; TODO asynchronizing call of rpc method
+
+;; TODO the implmentation of multi invoke is no so good.
+;; TODO just make it work.
+
 (ns clj-rpc.client
   (:require [clj-http.client :as http]
             [clj-http.util :as util]
@@ -24,15 +28,31 @@
     "Invoke functions with name func-name with arguments args on endpoint,
      notice because protocols do not support varargs, a more convinient way
      is using invoke-rpc function instead.")
+  (multi-invoke [endpoint invokes]
+     "Invoke mutli functions with name func-name with arguments args on endpoint,
+     the invokes must a collection of func-name args like [fun1 arg1 fun2 arg2]
+     notice because protocols do not support varargs, a more convinient way
+     is using multi-invoke-rpc function instead.")
   (help [endpoint]
     "Returns the list of the functions that endpoint support."))
 
 (def ^:private content-type "charset=UTF-8")
 
+(defn- mk-query-body
+  "make http invoke body.
+   the body has two kinds
+   1. a map means a single invoke.
+   2. a collection means multi invokes."
+  [method-name args & method-args]
+  (if (seq method-args)
+    (map (fn [[meth ps]] {:method meth :params ps})
+         (partition 2 2 (concat [method-name args] method-args)))
+    {:method method-name :params args}))
+
 (defn- mk-query
   "make query object to send to endpoint."
-  [f-encode method-name args]
-  (let [body (f-encode {:method method-name :params args}) ]
+  [f-encode method-name args & method-args]
+  (let [body (f-encode (apply mk-query-body method-name args method-args)) ]
     {:body body :content-type content-type}))
 
 (defn get-response-value
@@ -40,29 +60,43 @@
   [m key]
   (or (get m key) (get m (name key))))
 
-(defn- remote-call
-  "invoke a method with args using http"
-  [endpoint-url f-read f-write  method-name args]
-  (let [query (mk-query f-write method-name args)
-        response (->> query
-         (http/post endpoint-url)
-         :body
-         (f-read))
-        error (get-response-value response :error)]
-    (logging/debug "query:" query " response:" response)
+(defn- get-single-invoke-result
+  "get the response result, the response must be a map"
+  [response-m]
+  (let [error (get-response-value response-m :error)]
     (if (not error)
-      (get-response-value response :result)
+      (get-response-value response-m :result)
       (throw (RuntimeException. (str  "error cdoe : "
                                       (get-response-value error :code)
                                       " message: "
                                       (get-response-value error :message)))))))
+
+(defn get-invoke-result
+  "get the invoke result , the result has two kinds:
+  1. a map means a single invoke
+  2. a collection means multi invoke"
+  [response]
+  (if (map? response)
+    (get-single-invoke-result response)
+    (map get-single-invoke-result response)))
+
+(defn- remote-call
+  "invoke a method with args using http"
+  [endpoint-url f-read f-write  method-name args & method-args]
+  (let [query (apply mk-query f-write method-name args method-args)
+        response (->> query
+         (http/post endpoint-url)
+         :body
+         (f-read))]
+    (logging/debug "query:" query " response:" response)
+    (get-invoke-result response)))
 
 (defn- remote-help
   "get all the commands of the server support
    return a collection of map like
    [{:name \"conj\" :doc \"doc\" :arglist \"([x] [x y])} ... ]"
   [endpoint-url f-read]
-  (-> (http/get endpoint-url)
+  (-> (http/post endpoint-url)
       :body
       (f-read)))
 
@@ -76,11 +110,22 @@
     (let [url (format "http://%s:%d/%s" server port on-wire)]
       (reify RpcEndpoint
         (invoke [endpoint method-name args]
-          (remote-call (str url "/invoke") f-decode f-encode method-name args))
+            (remote-call (str url "/invoke") f-decode f-encode method-name args))
+        (multi-invoke [endpoint invokes]
+            (apply remote-call
+                   (str url "/invoke") f-decode f-encode invokes))
         (help [_]
-          (remote-help (str url "/help") f-decode))))))
+              (remote-help (str url "/help") f-decode))))))
 
 (defn invoke-rpc
-  "Invoke remote func-name on endpoint with args."
+  "Invoke remote func-name on endpoint with args.
+  example: (invoke-rpc func1 arg1 arg2 ...)"
   [endpoint func-name & args]
   (invoke endpoint func-name args))
+
+(defn multi-invoke-rpc
+  "Invoke multi remote func-name on endpoint with args.
+  args must be a collection
+  example: (multi-invoke-rpc fun1 [arg1 arg2 ...] func2 [arg1 arg2 ...]])"
+  [endpoint & func-args]
+  (multi-invoke endpoint func-args))
