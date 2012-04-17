fe378b0d992413c7436d508443990f78b44bc7ea
sukuizhang <skz@skzdeMacBook-Pro.local>
2012-04-17 10:31:59 +0800

    升级版本号

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-rpc "0.2.4-SNAPSHOT"
+(defproject clj-rpc "0.2.4"
             :description "simple rpc using clojure"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojure/tools.logging "0.2.3"]
