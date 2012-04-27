d76e9696a054582c579d43bd85e5efff72eb97b5
sukuizhang <skz@mainsvr.volcano>
2012-04-27 16:55:56 +0800

    release 0.2.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-rpc "0.2.7-SNAPSHOT"
+(defproject clj-rpc "0.2.7"
             :description "simple rpc using clojure"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [org.clojure/tools.logging "0.2.3"]
