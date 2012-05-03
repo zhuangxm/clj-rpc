228574a3fc1bc1c0016dd004917912c05a6d7424
sukuizhang <skz@mainsvr.volcano>
2012-05-03 09:12:00 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-rpc "0.2.8-SNAPSHOT"
+(defproject clj-rpc "0.2.8"
             :description "simple rpc using clojure"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [org.clojure/tools.logging "0.2.3"]
