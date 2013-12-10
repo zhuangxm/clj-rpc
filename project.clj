64b644a368dfce62ec49175ef28bb2162a5f463f
Martin <zxm@basecity.com>
2013-12-10 18:43:48 +1300

    upgrade clj-http ring and other depences, upgade clj-rpc to 0.3.2

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject clj-rpc "0.3.1"
+(defproject clj-rpc "0.3.2"
             :description "simple rpc using clojure"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [org.clojure/tools.logging "0.2.3"]
-                           [clj-http "0.2.1"]
-                           [compojure "1.1.1"]
-                           [ring "1.1.1"]
-                           [cheshire "4.0.1"]
+                           [clj-http "0.7.8"]
+                           [compojure "1.1.6"]
+                           [ring "1.2.1"]
+                           [cheshire "5.2.0"]
                            [easyconf "0.1.1"]]
             :dev-dependencies [[log4j "1.2.16"]
                                [midje "1.3-alpha5"]])
-
